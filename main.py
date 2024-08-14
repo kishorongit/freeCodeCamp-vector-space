dd45bd8747fceb3e5f8932f38ed8686a1b20cd91
Kishor Subedi <subedi.kishor1987@gmail.com>
2024-08-14 13:57:01 +0545

    feat: Add functionality for object instantiation string

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ class R2Vector:
     def __str__(self):
         return str(tuple(getattr(self, i) for i in vars(self)))
 
+    def __repr__(self):
+        arg_list = [f'{key}={val}' for key, val in vars(self).items()]
+        args = ', '.join(arg_list)
+        return f'{self.__class__.__name__}({args})'
+
 
 class R3Vector(R2Vector):
     def __init__(self, *, x, y, z):
