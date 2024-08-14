a5e9eec3c0c3b7f445e31e6882069669f1905627
Kishor Subedi <subedi.kishor1987@gmail.com>
2024-08-14 15:58:51 +0545

    feat: Implement cross product for 3-D vector

--- main.py
+++ main.py
@@ -64,3 +64,14 @@ class R3Vector(R2Vector):
     def __init__(self, *, x, y, z):
         super().__init__(x=x, y=y)
         self.z = z
+
+    def cross(self, other):
+        if type(self) != type(other):
+            return NotImplemented
+        kwargs = {
+            'x': self.y * other.z - self.z * other.y,
+            'y': self.z * other.x - self.x * other.z,
+            'z': self.x * other.y - self.y * other.x
+        }
+
+        return self.__class__(**kwargs)
